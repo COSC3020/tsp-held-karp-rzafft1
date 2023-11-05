07212dc20c08a8330e556f77b7757c8ce0bbcfaa
rzafft1 <rzafft1@uwyo.edu>
2023-11-05 00:20:14 -0600

    brute force is absolutley HORRIBLE  with an np-hard problem like this. this was more for my own  understanding, im working on a different approach.

--- code.js
+++ code.js
@@ -1,3 +1,62 @@
-function tsp_hk(distance_matrix) {
-    return -1;
+function swap (x, a, b)
+{
+    let tmp = x[a]
+    x[a] = x[b]
+    x[b] = tmp
 }
+
+function getPermutations (arr, index, permutations)
+{
+    if (index == arr.length)
+    {
+        permutations.push([...arr])
+    }
+    else 
+    {
+        for (var i = index; i < arr.length; i++)
+        {
+            swap(arr, index, i)
+            getPermutations(arr, index+1, permutations)
+            swap(arr, index, i)
+        } 
+    }
+    return permutations
+}
+
+function tsp_hk(dm)
+{
+    cities = []
+    for (city in dm)
+    {
+        cities.push(parseInt(city))
+    }
+    let perms = []
+    let permutations = getPermutations(cities,0,perms)
+    let distance = Infinity
+    for (let i = 0; i < permutations.length; i++)
+    {
+        let cities = permutations[i]
+        let sumdist = 0
+        for (let i = 0; i < cities.length; i++)
+        {
+            let city = cities[i]
+            let nextcity = cities[i+1]
+            if (nextcity != undefined) 
+            {
+                sumdist += dm[city][nextcity]
+            }
+        }
+        distance = Math.min(distance,sumdist)
+    }
+    return distance
+}
+
+let dist = [[0,3,4,2,7],
+[3,0,4,6,3],
+[4,4,0,5,8],
+[2,6,5,0,6],
+[7,3,8,6,0]];
+
+shortestdistance = tsp_hk(dist)
+console.log("shortest path has distance : " + shortestdistance)
+
